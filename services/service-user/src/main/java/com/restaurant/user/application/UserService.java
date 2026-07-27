909262c4dfa532d13e35bde6b17c58b426f17eea
NDE18 <rodriguekongne@gmail.com>
2026-07-27 20:48:04 +0200

    ci: Pipeline service-user

--- services/service-user/src/main/java/com/restaurant/user/application/UserService.java
+++ services/service-user/src/main/java/com/restaurant/user/application/UserService.java
@@ -26,7 +26,6 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Slf4j
 @Transactional
-@Data
 public class UserService {
 
     private final UserRepository userRepository;
@@ -116,7 +115,7 @@ public class UserService {
         userRepository.save(user);
     }
 
-    /*private UserResponse toResponse(User user) {
+    private UserResponse toResponse(User user) {
         return UserResponse.builder()
                 .id(user.getId())
                 .email(user.getEmail())
@@ -131,5 +130,5 @@ public class UserService {
                         .collect(Collectors.toSet()))
                 .createdAt(user.getCreatedAt())
                 .build();
-    }*/
+    }
 }
