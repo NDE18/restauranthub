3676a6f1be6fada17a26872e0215772a66390df5
NDE18 <rodriguekongne@gmail.com>
2026-08-05 20:27:06 +0200

    pipeline menu

--- services/service-menu/src/main.ts
+++ services/service-menu/src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(`service-menu démarre sur le port ${port}`);
+  console.log(`Service menu démarre sur le port ${port}`);
 }
 
 bootstrap();
